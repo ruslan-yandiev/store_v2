05f4204450aaa175fbf5fe65a2d05aa8854d253f
ruslan <sqwer1986@gmail.com>
2020-03-10 22:26:50 +0300

    change config and views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,15 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+//= require jquery-1.11.0.min
+//= require bootstrap.min
+//= require typeahead.bundle
+//= require jquery.easydropdown
+//= require easydrop
+//= require memenu
+//= require mem
+//= require responsiveslides.min
+//= require responsive
+//= require imagezoom
+//= require jquery.flexslider
+//= require flexs
